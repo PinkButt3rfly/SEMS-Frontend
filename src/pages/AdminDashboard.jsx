07af25f43c275b554ec03b131dd352bd2fd56f4b
Olivia <ese.olivia24@gmail.com>
2025-04-07 19:31:00 -0400

    update

--- src/pages/AdminDashboard.jsx
+++ src/pages/AdminDashboard.jsx
@@ -1,16 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useAuth } from '../context/authContext'
 import AdminSidebar from '../components/dashboard/AdminSidebar'
 import Navbar from '../components/dashboard/Navbar'
 import { Outlet } from 'react-router-dom'
 
 const AdminDashboard = () => {
+  const { user } = useAuth()
+  const [isSidebarOpen, setSidebarOpen] = useState(false) 
+
   return (
     <div className="flex flex-col md:flex-row h-screen">
-     
-      <AdminSidebar />
+      
+      <div
+        className={`md:w-64 w-full bg-gray-800 text-white fixed top-0 left-0 h-full transition-all duration-300 ease-in-out ${
+          isSidebarOpen ? 'translate-x-0' : '-translate-x-full'
+        } md:translate-x-0 z-50 md:relative`} 
+      >
+        <AdminSidebar setSidebarOpen={setSidebarOpen} isSidebarOpen={isSidebarOpen} />
+      </div>
 
-      <div className="flex-1 h-screen bg-gray-200">
-        <Navbar />
+      
+      <div
+        className={`flex-1 h-screen bg-gray-200 transition-all duration-300 ease-in-out ${
+          isSidebarOpen ? 'md:ml-64 ml-0' : ''
+        }`} 
+      >
+        <Navbar /> 
         <div className="p-4">
           <Outlet />
         </div>
