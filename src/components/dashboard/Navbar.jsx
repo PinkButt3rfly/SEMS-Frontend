e9d34953a863749d6a229cbbe903a89b1084061a
Olivia <ese.olivia24@gmail.com>
2025-04-07 19:16:43 -0400

    update

--- src/components/dashboard/Navbar.jsx
+++ src/components/dashboard/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useAuth } from '../../context/authContext';
-import { FaBars } from 'react-icons/fa'; 
+import { FaBars } from 'react-icons/fa';
 
 const Navbar = ({ setSidebarOpen }) => {
   const { user, logout } = useAuth();
@@ -8,8 +8,8 @@ const Navbar = ({ setSidebarOpen }) => {
   return (
     <div className="flex items-center justify-between h-12 bg-pink-600 text-white px-4">
       
-      <button 
-        className="md:hidden text-white" 
+      <button
+        className="md:hidden text-white"
         onClick={() => setSidebarOpen(prev => !prev)} 
       >
         <FaBars />
@@ -19,7 +19,7 @@ const Navbar = ({ setSidebarOpen }) => {
       <p className="flex-1 text-center sm:text-left">Welcome {user.name}</p>
 
       
-      <button 
+      <button
         className="px-4 py-1 bg-gray-800 rounded-md hover:bg-pink-800 mr-6"
         onClick={logout}
       >
--- src/pages/AdminDashboard.jsx
+++ src/pages/AdminDashboard.jsx
@@ -7,25 +7,21 @@ import { Outlet } from 'react-router-dom';
 const AdminDashboard = () => {
   const { user } = useAuth();
   const [isSidebarOpen, setSidebarOpen] = useState(false); 
-  
+
   return (
     <div className="flex flex-col md:flex-row h-screen">
       {/* Sidebar */}
       <div
-        className={`md:w-64 w-full bg-gray-800 text-white fixed top-0 left-0 h-full transition-all duration-300 ease-in-out ${
-          isSidebarOpen ? 'translate-x-0' : '-translate-x-full'
-        } md:translate-x-0 z-50 md:relative`} 
+        className={`md:w-64 w-full bg-gray-800 text-white fixed top-0 left-0 h-full transition-transform duration-300 ease-in-out z-50 ${isSidebarOpen ? 'translate-x-0' : '-translate-x-full'} md:translate-x-0 md:relative`}
       >
         <AdminSidebar />
       </div>
 
       
       <div
-        className={`flex-1 h-screen bg-gray-200 transition-all duration-300 ease-in-out ${
-          isSidebarOpen ? 'md:ml-64 ml-0' : ''
-        }`} 
+        className={`flex-1 h-screen bg-gray-200 transition-all duration-300 ease-in-out ${isSidebarOpen ? 'md:ml-64 ml-0' : ''}`}
       >
-        <Navbar setSidebarOpen={setSidebarOpen} /> {/* Pass the function to Navbar */}
+        <Navbar setSidebarOpen={setSidebarOpen} /> 
         <div className="p-4">
           <Outlet />
         </div>
