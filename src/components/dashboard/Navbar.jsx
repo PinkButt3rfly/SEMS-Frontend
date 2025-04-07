8f085e84aeadcc1fc80722dd68d0b6811868f389
Olivia <ese.olivia24@gmail.com>
2025-04-07 19:26:49 -0400

    update

--- src/components/dashboard/Navbar.jsx
+++ src/components/dashboard/Navbar.jsx
@@ -1,32 +1,29 @@
-import React from 'react';
-import { useAuth } from '../../context/authContext';
-import { FaBars } from 'react-icons/fa';
+import React from 'react'
+import { useAuth } from '../../context/authContext'
+import { FaUserCircle } from 'react-icons/fa'
 
-const Navbar = ({ setSidebarOpen }) => {
-  const { user, logout } = useAuth();
+const Navbar = () => {
+  const { user, logout } = useAuth()
 
   return (
     <div className="flex items-center justify-between h-12 bg-pink-600 text-white px-4">
       
-      <button
-        className="md:hidden text-white"
-        onClick={() => setSidebarOpen(prev => !prev)} 
-      >
-        <FaBars />
-      </button>
-
-      
       <p className="flex-1 text-center sm:text-left">Welcome {user.name}</p>
 
       
-      <button
+      <button 
         className="px-4 py-1 bg-gray-800 rounded-md hover:bg-pink-800 mr-6"
         onClick={logout}
       >
         Logout
       </button>
-    </div>
-  );
-};
 
-export default Navbar;
+      
+      <div className="text-xl">
+        <FaUserCircle />
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
--- src/pages/AdminDashboard.jsx
+++ src/pages/AdminDashboard.jsx
@@ -1,33 +1,22 @@
-import React, { useState } from 'react';
-import { useAuth } from '../context/authContext';
-import AdminSidebar from '../components/dashboard/AdminSidebar';
-import Navbar from '../components/dashboard/Navbar';
-import { Outlet } from 'react-router-dom';
+import React from 'react'
+import AdminSidebar from '../components/dashboard/AdminSidebar'
+import Navbar from '../components/dashboard/Navbar'
+import { Outlet } from 'react-router-dom'
 
 const AdminDashboard = () => {
-  const { user } = useAuth();
-  const [isSidebarOpen, setSidebarOpen] = useState(false); 
-
   return (
     <div className="flex flex-col md:flex-row h-screen">
-      {/* Sidebar */}
-      <div
-        className={`md:w-64 w-full bg-gray-800 text-white fixed top-0 left-0 h-full transition-transform duration-300 ease-in-out z-50 ${isSidebarOpen ? 'translate-x-0' : '-translate-x-full'} md:translate-x-0 md:relative`}
-      >
-        <AdminSidebar />
-      </div>
+     
+      <AdminSidebar />
 
-      
-      <div
-        className={`flex-1 h-screen bg-gray-200 transition-all duration-300 ease-in-out ${isSidebarOpen ? 'md:ml-64 ml-0' : ''}`}
-      >
-        <Navbar setSidebarOpen={setSidebarOpen} /> 
+      <div className="flex-1 h-screen bg-gray-200">
+        <Navbar />
         <div className="p-4">
           <Outlet />
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default AdminDashboard;
+export default AdminDashboard
