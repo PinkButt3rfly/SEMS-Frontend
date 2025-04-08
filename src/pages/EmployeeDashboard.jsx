51a3cac293e8558f17efa918998baa1d26cb5709
Olivia <ese.olivia24@gmail.com>
2025-04-07 20:42:04 -0400

    update

--- src/pages/EmployeeDashboard.jsx
+++ src/pages/EmployeeDashboard.jsx
@@ -1,36 +1,37 @@
-import React, { useState } from 'react';
-import Sidebar from '../components/EmployeeDashboard/Sidebar';
-import Navbar from '../components/dashboard/Navbar';
-import { Outlet } from 'react-router-dom';
+import React, { useState } from 'react'
+import { useAuth } from '../context/authContext'
+import Sidebar from '../components/EmployeeDashboard/Sidebar'
+import Navbar from '../components/dashboard/Navbar'
+import { Outlet } from 'react-router-dom'
 
 const EmployeeDashboard = () => {
-  const [isSidebarOpen, setSidebarOpen] = useState(false); // Sidebar state to toggle visibility
+  const { user } = useAuth()
+  const [isSidebarOpen, setSidebarOpen] = useState(false) // Sidebar state to toggle visibility
 
   return (
     <div className="flex flex-col md:flex-row h-screen">
-      
       {/* Sidebar */}
       <div
-        className={`md:w-64 w-full bg-gray-800 text-white fixed top-0 left-0 h-full transition-all duration-300 ease-in-out ${
+        className={`fixed md:relative top-0 left-0 bottom-0 bg-gray-800 text-white h-full transition-all duration-300 ease-in-out ${
           isSidebarOpen ? 'translate-x-0' : '-translate-x-full'
-        } md:translate-x-0 z-50 md:relative`} // Sidebar toggle
+        } md:translate-x-0 w-64 z-50`}
       >
         <Sidebar />
       </div>
 
       {/* Main Content Area */}
       <div
-        className={`flex-1 h-screen bg-gray-200 transition-all duration-300 ease-in-out ${
+        className={`flex-1 bg-gray-200 h-full transition-all duration-300 ease-in-out ${
           isSidebarOpen ? 'ml-64' : ''
-        }`} // Shift content when sidebar is open
+        }`}
       >
-        <Navbar setSidebarOpen={setSidebarOpen} /> {/* Pass the function to Navbar */}
+        <Navbar setSidebarOpen={setSidebarOpen} />
         <div className="p-4">
           <Outlet />
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default EmployeeDashboard;
+export default EmployeeDashboard
